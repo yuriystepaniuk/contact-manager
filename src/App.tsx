72e8b413927bccc72ccf101ecd38e8b8976e7b71
Yuriy Stepaniuk <yur.stepanyuk@gmail.com>
2025-04-30 23:05:28 +0300

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>;
-}
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ContactsPage from "./pages/ContactsPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ContactsPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
